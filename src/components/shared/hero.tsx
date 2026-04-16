2c0c67b75d127c8d71a272858b1952a5d2cf811f
MatchaTi <tehmatcha28@gmail.com>
2026-04-16 14:58:32 +0800

    chore(ui): update hero component

--- src/components/shared/hero.tsx
+++ src/components/shared/hero.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function Hero({ heading, badge, description, children }: Props) {
   return (
-    <div className='space-y-12 mt-10 lg:mt-20'>
+    <div className='space-y-12 my-10 lg:my-20'>
       <Heading
         level='h2'
         className='font-sans-jp bg-foreground w-fit text-background'
@@ -23,7 +23,7 @@ export default function Hero({ heading, badge, description, children }: Props) {
         <div className='w-4 aspect-square border absolute bg-background bottom-0 left-0 translate-y-1/2 -translate-x-1/2'></div>
         <div className='w-4 aspect-square border absolute bg-background bottom-0 right-0 translate-y-1/2 translate-x-1/2'></div>
       </div>
-      <div className='space-y-8 px-4'>
+      <div className='space-y-8'>
         <p className='mt-20'>{description}</p>
         {children && <div>{children}</div>}
       </div>
